b89b4101f26cb7070bd5710f1828a3340bc1a659
jademonroe06 <mikaelajael777@gmail.com>
2026-04-09 10:14:59 +0200

    trabajando en seguir implementando microservicios, gateway-service y iam-service más completos

---
+++ microservices-platform/bff/src/main/java/com/aula/microservices/mobile/MobileBffApplication.java
@@ -0,0 +1,11 @@
+package com.aula.microservices.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileBffApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MobileBffApplication.class, args);
+    }
+}
